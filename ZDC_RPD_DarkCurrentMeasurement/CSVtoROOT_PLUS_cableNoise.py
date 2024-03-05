5cf3023d4d563fe03609027c79917ea5fe70b422
ugtok <ufukgtok@gmail.com>
2024-03-05 09:23:53 +0100

    FFT funciton
    Update on FFT function to check the magnitude of cable noise

--- ZDC_RPD_DarkCurrentMeasurement/CSVtoROOT_PLUS_cableNoise.py
+++ ZDC_RPD_DarkCurrentMeasurement/CSVtoROOT_PLUS_cableNoise.py
@@ -7,9 +7,16 @@ Description: Noise Measurement for the Signal Cables (lemo) on the Plus Side, un
 Script usage: "python CSVtoRoot.py --section"
         e.g   python CSVtoRoot.py RPD
 '''
+import numpy as np
 import ROOT
 import sys
 import os
+import array
+import matplotlib.pyplot as plt
+
+# Load the necessary ROOT libraries
+ROOT.gSystem.Load("libMathCore")
+ROOT.gSystem.Load("libMathMore")
 
 fileNames_Plus_Cable_Noise_RPD = ["ZDC_Plus_CableNoiseCSVs/1522269-rpd1.csv", "ZDC_Plus_CableNoiseCSVs/1522270-rpd2.csv", "ZDC_Plus_CableNoiseCSVs/1522271-rpd3.csv",
 "ZDC_Plus_CableNoiseCSVs/1522272-rpd4.csv","ZDC_Plus_CableNoiseCSVs/1522273-rpd5.csv","ZDC_Plus_CableNoiseCSVs/1522274-rpd6.csv","ZDC_Plus_CableNoiseCSVs/1522275-rpd7.csv",
@@ -17,9 +24,9 @@ fileNames_Plus_Cable_Noise_RPD = ["ZDC_Plus_CableNoiseCSVs/1522269-rpd1.csv", "Z
 "ZDC_Plus_CableNoiseCSVs/1522280-rpd12.csv","ZDC_Plus_CableNoiseCSVs/1522281-rpd13.csv","ZDC_Plus_CableNoiseCSVs/1522282-rpd14.csv","ZDC_Plus_CableNoiseCSVs/1522283-rpd15.csv",
 "ZDC_Plus_CableNoiseCSVs/1522284-rpd16.csv"]
 
-fileNames_Plus_Cable_Noise_EM = ["ZDC_Plus_CableNoiseCSVs/1522343-em1.csv","ZDC_Plus_CableNoiseCSVs/1522344-em2.csv","ZDC_Plus_CableNoiseCSVs/1522345-em3.csv","ZDC_Plus_CableNoiseCSVs/1522346-em4.csv"]
+fileNames_Minus_Cable_Noise_EM = ["ZDC_Plus_CableNoiseCSVs/1522343-em1.csv","ZDC_Plus_CableNoiseCSVs/1522344-em2.csv","ZDC_Plus_CableNoiseCSVs/1522345-em3.csv","ZDC_Plus_CableNoiseCSVs/1522346-em4.csv","ZDC_Plus_CableNoiseCSVs/1522347-em5.csv"]
 
-fileNames_Plus_Cable_Noise_HAD = ["ZDC_Plus_CableNoiseCSVs/1522348-had1.csv","ZDC_Plus_CableNoiseCSVs/1522266-had2.csv","ZDC_Plus_CableNoiseCSVs/1522267-had3.csv","ZDC_Plus_CableNoiseCSVs/1522268-had4.csv"]
+fileNames_Minus_Cable_Noise_HAD = ["ZDC_Plus_CableNoiseCSVs/1522348-had1.csv","ZDC_Plus_CableNoiseCSVs/1522266-had2.csv","ZDC_Plus_CableNoiseCSVs/1522267-had3.csv","ZDC_Plus_CableNoiseCSVs/1522268-had4.csv"]
 
 fileNames_Test = ["ZDC_Plus_CableNoiseCSVs/1522285-spare.csv"]
 
@@ -35,11 +42,14 @@ count = 0
 if section == "RPD":
         fileNames = fileNames_Plus_Cable_Noise_RPD
 elif section == "HAD":
-        fileNames = fileNames_Plus_Cable_Noise_HAD
+        fileNames = fileNames_Minus_Cable_Noise_HAD
 elif section == "EM":
-        fileNames = fileNames_Plus_Cable_Noise_EM
+        fileNames = fileNames_Minus_Cable_Noise_EM
 elif section == "Test":
         fileNames = fileNames_Test
+else:
+        print("Invalid section provided! Please use RPD, HAD, EM or Test.")
+        sys.exit(1) 
 
 for fileName in fileNames:
         count = count +1
@@ -49,20 +59,34 @@ for fileName in fileNames:
         rootfileName = "ZDC_Cable_%s/%s_rootFiles/%s_Channel_%s.root"%(section,section,section,count)
         treeName = "Channel_%s"%str(count)
 
-        histo1D = df.Histo1D(("Channel_%s"%count,"%s_Channel_%s; second (ms);Volt (mV)"%(section,count),150,-0.0002,0.001),"Volt")
-
-	#histo1D.Fit("gaus","V","",0.0,0.0009)
-	print("Channel_%s ------>"%str(count),histo1D.Integral())
-
-	#measure = histo1D.Integral()/50
-	
-	#with open("Output_%s.txt"%section,"a") as text_file:
-	#	text_file.write("Integral of Channel_%s == %s\n"%(str(count),measure))
+        histo_csv = df.Histo1D(("Channel_%s"%count,"%s_Channel_%s; second (ms);Volt (mV)"%(section,count),200,0.0,0.06),"Volt")
         
-	df.Snapshot(treeName,rootfileName,"",snapshotOptions)
+        # Get number of bins in the histogram
+        n_bins = histo_csv.GetNbinsX()
+
+        data = [histo_csv.GetBinContent(i) for i in range(1, n_bins + 1)]
+
+        # Perform FFT
+        fft_result = np.fft.fft(data)
+        magnitude = np.abs(fft_result)
+        print(magnitude)
+
+        # Draw magnitude plots per frequency bins
+        plt.figure()
+        plt.plot(np.arange(0, len(magnitude)), magnitude)
+        plt.title("Magnitude Spectrum of FFT Result")
+        plt.xlabel("Frequency Bin")
+        plt.ylabel("Magnitude")
+        plt.grid(True)
+        plt.savefig("ZDC_Cable_%s/Histograms/%s_Channel_%s_FFT_Magnitude.pdf"%(section,section,str(count)))
+        plt.close()   
+        
+
+        # Draw the Volt vs second 1D histograms
+        df.Snapshot(treeName,rootfileName,"",snapshotOptions)
         f = ROOT.TFile.Open(rootfileName)
         c = ROOT.TCanvas()
         #c.SetLogx()
         c.SetLogy()
-        histo1D.Draw("pl")
+        histo_csv.Draw("pl")
         c.SaveAs("ZDC_Cable_%s/Histograms/%s_Channel_%s.pdf"%(section,section,str(count)))
